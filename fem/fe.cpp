e4296b316ea1ad05838d5308c97aa6560998ce1e
Veselin Dobrev <dobrev@llnl.gov>
2011-06-07 13:39:01 -0700

    Deleted trailing whitespace.

--- fem/fe.cpp
+++ fem/fe.cpp
@@ -3892,38 +3892,36 @@ RT0HexFiniteElement::RT0HexFiniteElement()
    Nodes.IntPoint(5).x = 0.5;
    Nodes.IntPoint(5).y = 0.5;
    Nodes.IntPoint(5).z = 1.0;
-     
 }
 
 void RT0HexFiniteElement::CalcVShape(const IntegrationPoint &ip,
                                      DenseMatrix &shape) const
 {
    double x = ip.x, y = ip.y, z = ip.z;
-   // z = 0 
+   // z = 0
    shape(0,0) = 0.;
    shape(0,1) = 0.;
    shape(0,2) = 1. - z;
-   // y = 0 
+   // y = 0
    shape(1,0) = 0.;
    shape(1,1) = 1. - y;
    shape(1,2) = 0.;
-   // x = 1 
+   // x = 1
    shape(2,0) = x;
    shape(2,1) = 0.;
    shape(2,2) = 0.;
-   // y = 1 
+   // y = 1
    shape(3,0) = 0.;
    shape(3,1) = y;
    shape(3,2) = 0.;
-   // x = 0 
+   // x = 0
    shape(4,0) = 1. - x;
    shape(4,1) = 0.;
    shape(4,2) = 0.;
-   // z = 1 
+   // z = 1
    shape(5,0) = 0.;
    shape(5,1) = 0.;
    shape(5,2) = z;
-   
 }
 
 void RT0HexFiniteElement::CalcDivShape(const IntegrationPoint &ip,
