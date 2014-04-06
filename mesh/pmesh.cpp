bf97eba964e4e09d19dbdd4d9198304bdd418c11
Thomas A. Brunner <brunner6@llnl.gov>
2014-04-06 08:01:05 -0700

    Fixed mismatch between types sent to MPI
    We were taking the address of the array, instead
    of the address of the data pointed to by the array.

--- mesh/pmesh.cpp
+++ mesh/pmesh.cpp
@@ -1007,11 +1007,11 @@ void ParMesh::ExchangeFaceNbrNodes()
          int nbr_rank = GetFaceNbrRank(fn);
          int tag = 0;
 
-         MPI_Isend(&send_vertices[send_face_nbr_vertices.GetI()[fn]],
+         MPI_Isend(send_vertices[send_face_nbr_vertices.GetI()[fn]](),
                    3*send_face_nbr_vertices.RowSize(fn),
                    MPI_DOUBLE, nbr_rank, tag, MyComm, &send_requests[fn]);
 
-         MPI_Irecv(&face_nbr_vertices[face_nbr_vertices_offset[fn]],
+         MPI_Irecv(face_nbr_vertices[face_nbr_vertices_offset[fn]](),
                    3*(face_nbr_vertices_offset[fn+1] -
                       face_nbr_vertices_offset[fn]),
                    MPI_DOUBLE, nbr_rank, tag, MyComm, &recv_requests[fn]);
