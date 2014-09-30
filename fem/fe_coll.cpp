6d36e15b0e9a8f2665c7dc8fdb7ab414633c7234
Tzanio <tzanio@llnl.gov>
2014-09-30 14:04:10 -0700

    Added names for Local_L2* and Local_H1* finite element collections that are used by some SStruct examples in hypre (and Braid).

--- fem/fe_coll.cpp
+++ fem/fe_coll.cpp
@@ -1645,6 +1645,16 @@ Local_FECollection::Local_FECollection(const char *fe_name)
       GeomType = Geometry::CUBE;
       Local_Element = new Nedelec1HexFiniteElement;
    }
+   else if (!strncmp(fe_name, "H1_", 3))
+   {
+      GeomType = Geometry::SQUARE;
+      Local_Element = new H1_QuadrilateralElement(atoi(fe_name + 7));
+   }
+   else if (!strncmp(fe_name, "L2_", 3))
+   {
+      GeomType = Geometry::SQUARE;
+      Local_Element = new L2_QuadrilateralElement(atoi(fe_name + 7));
+   }
    else
    {
       cerr << "Local_FECollection::Local_FECollection : fe_name = "
