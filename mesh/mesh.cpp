84c39f9762a07b9ed56e39b4382dac7c1c800cf4
Tzanio <tzanio@llnl.gov>
2015-12-05 13:19:45 -0800

    Forgot some newlines

--- mesh/mesh.cpp
+++ mesh/mesh.cpp
@@ -117,7 +117,8 @@ void Mesh::PrintCharacteristics(Vector *Vh, Vector *Vk, std::ostream &out)
 
    if (dim == 1)
    {
-      out << "Number of vertices : " << GetNV() << '\n'
+      out << '\n'
+          << "Number of vertices : " << GetNV() << '\n'
           << "Number of elements : " << GetNE() << '\n'
           << "Number of bdr elem : " << GetNBE() << '\n'
           << "h_min              : " << h_min << '\n'
@@ -125,19 +126,21 @@ void Mesh::PrintCharacteristics(Vector *Vh, Vector *Vk, std::ostream &out)
    }
    else if (dim == 2)
    {
-      out  << "Number of vertices : " << GetNV() << '\n'
-           << "Number of edges    : " << GetNEdges() << '\n'
-           << "Number of elements : " << GetNE() << '\n'
-           << "Number of bdr elem : " << GetNBE() << '\n'
-           << "Euler Number       : " << EulerNumber2D() << '\n'
-           << "h_min              : " << h_min << '\n'
-           << "h_max              : " << h_max << '\n'
-           << "kappa_min          : " << kappa_min << '\n'
-           << "kappa_max          : " << kappa_max << '\n';
+      out << '\n'
+          << "Number of vertices : " << GetNV() << '\n'
+          << "Number of edges    : " << GetNEdges() << '\n'
+          << "Number of elements : " << GetNE() << '\n'
+          << "Number of bdr elem : " << GetNBE() << '\n'
+          << "Euler Number       : " << EulerNumber2D() << '\n'
+          << "h_min              : " << h_min << '\n'
+          << "h_max              : " << h_max << '\n'
+          << "kappa_min          : " << kappa_min << '\n'
+          << "kappa_max          : " << kappa_max << '\n';
    }
    else
    {
-      out << "Number of vertices : " << GetNV() << '\n'
+      out << '\n'
+          << "Number of vertices : " << GetNV() << '\n'
           << "Number of edges    : " << GetNEdges() << '\n'
           << "Number of faces    : " << GetNFaces() << '\n'
           << "Number of elements : " << GetNE() << '\n'
