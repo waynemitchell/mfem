39c6d260358ecfcadc44a337e506cbe2ae2745d1
Thomas R. Benson <benson31@llnl.gov>
2016-08-04 10:49:20 -0700

    Examples now build in both serial and parallel

--- cmake/modules/MfemCmakeUtilities.cmake
+++ cmake/modules/MfemCmakeUtilities.cmake
@@ -7,3 +7,28 @@ function(convert_filenames_to_full_paths NAMES)
   endforeach()
   set(${NAMES} ${tmp_names} PARENT_SCOPE)
 endfunction()
+
+function(add_mfem_examples EXE_SRCS)
+  foreach(SRC_FILE IN LISTS ${EXE_SRCS})
+    get_filename_component(SRC_FILENAME ${SRC_FILE} NAME)
+    
+    string(REPLACE ".cpp" ".exe" EXE_NAME ${SRC_FILENAME})
+    add_executable(${EXE_NAME} ${SRC_FILE})
+    
+    target_link_libraries(${EXE_NAME} mfem)
+    if (MFEM_USE_MPI)
+      target_link_libraries(${EXE_NAME} ${MPI_CXX_LIBRARIES})
+    endif()
+    
+    # Language-specific include directories:
+    target_include_directories(${EXE_NAME} PRIVATE ${MPI_CXX_INCLUDE_PATH})
+    if (MPI_CXX_COMPILE_FLAGS)
+      target_compile_options(${EXE_NAME} PRIVATE ${MPI_CXX_COMPILE_FLAGS})
+    endif()
+    
+    if (MPI_CXX_LINK_FLAGS)
+      set_target_properties(${EXE_NAME} PROPERTIES
+        LINK_FLAGS "${MPI_CXX_LINK_FLAGS}")
+    endif()
+  endforeach(SRC_FILE)
+endfunction()
