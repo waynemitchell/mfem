55d913a0fbc284dfaea9fa8faca2f8ee2921fb66
Thomas R. Benson <benson31@llnl.gov>
2016-08-10 14:19:39 -0700

    Add miniapps; add most of the tests. Miniapps tests not yet complete

--- config/cmake/modules/FindSuiteSparse.cmake
+++ config/cmake/modules/FindSuiteSparse.cmake
@@ -14,11 +14,10 @@
 # UMFPack is the most obvious dependency, so start there.
 find_path(UMFPACK_INCLUDE_DIRS umfpack.h
   HINTS ${SuiteSparse_DIR} $ENV{SuiteSparse_DIR}
-  PATH_SUFFIXES include
+  PATH_SUFFIXES suitesparse include
   NO_DEFAULT_PATH
   DOC "Directory where SuiteSparse headers live.")
-find_path(UMFPACK_INCLUDE_DIRS umfpack.h
-  PATH_SUFFIXES suitesparse)
+find_path(UMFPACK_INCLUDE_DIRS umfpack.h)
 
 find_library(UMFPACK_LIBRARY umfpack
   HINTS ${SuiteSparse_DIR} $ENV{SuiteSparse_DIR}
@@ -35,19 +34,18 @@ endif()
 
 # Now using umfpack as a guide, find the rest:
 find_path(AMD_INCLUDE_DIRS amd.h
-  HINTS
-    ${UMFPACK_INCLUDE_DIRS}
-    ${SuiteSparse_DIR}/include
-    $ENV{SuiteSparse_DIR}/include
-  PATH_SUFFIXES suitesparse
+  HINTS ${UMFPACK_INCLUDE_DIRS} ${SuiteSparse_DIR} $ENV{SuiteSparse_DIR}
+  PATH_SUFFIXES suitesparse include
+  NO_DEFAULT_PATH
   DOC "Directory where SuiteSparse headers live.")
+find_path(AMD_INCLUDE_DIRS amd.h)
 
 find_library(AMD_LIBRARY amd
-  HINTS
-    ${UMFPACK_LIB_DIR}
-    ${SuiteSparse_DIR}/lib
-    $ENV{SuiteSparse_DIR}/lib
+  HINTS ${UMFPACK_LIB_DIR} ${SuiteSparse_DIR} $ENV{SuiteSparse_DIR}
+  PATH_SUFFIXES lib
+  NO_DEFAULT_PATH
   DOC "The Amd Library.")
+find_library(AMD_LIBRARY amd)
 
 if(CMAKE_VERSION VERSION_GREATER 2.8.11)
   get_filename_component(AMD_LIB_DIR ${AMD_LIBRARY} DIRECTORY)
@@ -55,27 +53,20 @@ else()
   get_filename_component(AMD_LIB_DIR ${AMD_LIBRARY} PATH)
 endif()
 
-#message("AMD_INCLUDE_DIRS=${AMD_INCLUDE_DIRS}")
-#message("AMD_LIBRARY=${AMD_LIBRARY}")
-#message("AMD_LIB_DIR=${AMD_LIB_DIR}")
-
 # CHOLMOD
 find_path(CHOLMOD_INCLUDE_DIRS cholmod.h
-  HINTS
-    ${UMFPACK_INCLUDE_DIRS}
-    ${AMD_INCLUDE_DIRS}
-    ${SuiteSparse_DIR}/include
-    $ENV{SuiteSparse_DIR}/include
-  PATH_SUFFIXES suitesparse
+  HINTS ${UMFPACK_INCLUDE_DIRS} ${AMD_INCLUDE_DIRS} ${SuiteSparse_DIR} $ENV{SuiteSparse_DIR}
+  PATH_SUFFIXES suitesparse include
+  NO_DEFAULT_PATH
   DOC "Directory where SuiteSparse headers live.")
+find_path(CHOLMOD_INCLUDE_DIRS cholmod.h)
 
 find_library(CHOLMOD_LIBRARY cholmod
-  HINTS
-    ${UMFPACK_LIB_DIR}
-    ${AMD_LIB_DIR}
-    ${SuiteSparse_DIR}/lib
-    $ENV{SuiteSparse_DIR}/lib
+  HINTS ${UMFPACK_LIB_DIR} ${AMD_LIB_DIR} ${SuiteSparse_DIR} $ENV{SuiteSparse_DIR}
+  PATH_SUFFIXES lib
+  NO_DEFAULT_PATH
   DOC "The Cholmod Library.")
+find_library(CHOLMOD_LIBRARY cholmod)
 
 if(CMAKE_VERSION VERSION_GREATER 2.8.11)
   get_filename_component(CHOLMOD_LIB_DIR ${CHOLMOD_LIBRARY} DIRECTORY)
@@ -83,29 +74,24 @@ else()
   get_filename_component(CHOLMOD_LIB_DIR ${CHOLMOD_LIBRARY} PATH)
 endif()
 
-#message("CHOLMOD_INCLUDE_DIRS=${CHOLMOD_INCLUDE_DIRS}")
-#message("CHOLMOD_LIBRARY=${CHOLMOD_LIBRARY}")
-#message("CHOLMOD_LIB_DIR=${CHOLMOD_LIB_DIR}")
-
 # COLAMD
 find_path(COLAMD_INCLUDE_DIRS colamd.h
   HINTS
-    ${UMFPACK_INCLUDE_DIRS}
-    ${AMD_INCLUDE_DIRS}
-    ${CHOLMOD_INCLUDE_DIRS}
-    ${SuiteSparse_DIR}/include
-    $ENV{SuiteSparse_DIR}/include
-  PATH_SUFFIXES suitesparse
+  ${UMFPACK_INCLUDE_DIRS} ${AMD_INCLUDE_DIRS} ${CHOLMOD_INCLUDE_DIRS}
+  ${SuiteSparse_DIR} $ENV{SuiteSparse_DIR}
+  PATH_SUFFIXES suitesparse include
+  NO_DEFAULT_PATH
   DOC "Directory where SuiteSparse headers live.")
+find_path(COLAMD_INCLUDE_DIRS colamd.h)
 
 find_library(COLAMD_LIBRARY colamd
   HINTS
-    ${UMFPACK_LIB_DIR}
-    ${AMD_LIB_DIR}
-    ${CHOLMOD_LIB_DIR}
-    ${SuiteSparse_DIR}/lib
-    $ENV{SuiteSparse_DIR}/lib
+    ${UMFPACK_LIB_DIR} ${AMD_LIB_DIR} ${CHOLMOD_LIB_DIR}
+    ${SuiteSparse_DIR} $ENV{SuiteSparse_DIR}
+  PATH_SUFFIXES lib
+  NO_DEFAULT_PATH
   DOC "The Colamd Library.")
+find_library(COLAMD_LIBRARY colamd)
 
 if(CMAKE_VERSION VERSION_GREATER 2.8.11)
   get_filename_component(COLAMD_LIB_DIR ${COLAMD_LIBRARY} DIRECTORY)
@@ -113,32 +99,26 @@ else()
   get_filename_component(COLAMD_LIB_DIR ${COLAMD_LIBRARY} PATH)
 endif()
 
-#message("COLAMD_INCLUDE_DIRS=${COLAMD_INCLUDE_DIRS}")
-#message("COLAMD_LIBRARY=${COLAMD_LIBRARY}")
-#message("COLAMD_LIB_DIR=${COLAMD_LIB_DIR}")
-
-
 # SuiteSparseConfig
 find_path(CONFIG_INCLUDE_DIRS SuiteSparse_config.h
   HINTS
-    ${UMFPACK_INCLUDE_DIRS}
-    ${AMD_INCLUDE_DIRS}
-    ${COLAMD_INCLUDE_DIRS}
-    ${CHOLMOD_INCLUDE_DIRS}
-    ${SuiteSparse_DIR}/include
-    $ENV{SuiteSparse_DIR}/include
-  PATH_SUFFIXES suitesparse
+  ${UMFPACK_INCLUDE_DIRS} ${AMD_INCLUDE_DIRS}
+  ${COLAMD_INCLUDE_DIRS} ${CHOLMOD_INCLUDE_DIRS}
+  ${SuiteSparse_DIR} $ENV{SuiteSparse_DIR}
+  PATH_SUFFIXES suitesparse include
+  NO_DEFAULT_PATH
   DOC "Directory where SuiteSparse headers live.")
+find_path(CONFIG_INCLUDE_DIRS SuiteSparse_config.h)
 
 find_library(CONFIG_LIBRARY suitesparseconfig
   HINTS
-    ${UMFPACK_LIB_DIR}
-    ${AMD_LIB_DIR}
-    ${COLAMD_LIB_DIR}
-    ${CHOLMOD_LIB_DIR}
-    ${SuiteSparse_DIR}/lib
-    $ENV{SuiteSparse_DIR}/lib
-  DOC "The Spqr Library.")
+  ${UMFPACK_LIB_DIR} ${AMD_LIB_DIR}
+  ${COLAMD_LIB_DIR} ${CHOLMOD_LIB_DIR}
+  ${SuiteSparse_DIR} $ENV{SuiteSparse_DIR}
+  PATH_SUFFIXES lib
+  NO_DEFAULT_PATH  
+  DOC "The SuiteSparseConfig Library.")
+find_library(CONFIG_LIBRARY suitesparseconfig)
 
 if(CMAKE_VERSION VERSION_GREATER 2.8.11)
   get_filename_component(CONFIG_LIB_DIR ${CONFIG_LIBRARY} DIRECTORY)
@@ -148,13 +128,13 @@ endif()
 
 find_library(KLU_LIBRARY klu
   HINTS
-    ${UMFPACK_LIB_DIR}
-    ${AMD_LIB_DIR}
-    ${COLAMD_LIB_DIR}
-    ${CHOLMOD_LIB_DIR}
-    ${SuiteSparse_DIR}/lib
-    $ENV{SuiteSparse_DIR}/lib
+  ${UMFPACK_LIB_DIR} ${AMD_LIB_DIR}
+  ${COLAMD_LIB_DIR} ${CHOLMOD_LIB_DIR}
+  ${SuiteSparse_DIR} $ENV{SuiteSparse_DIR}
+  PATH_SUFFIXES lib
+  NO_DEFAULT_PATH
   DOC "The KLU Library.")
+find_library(KLU_LIBRARY klu)
 
 if(CMAKE_VERSION VERSION_GREATER 2.8.11)
   get_filename_component(KLU_LIB_DIR ${KLU_LIBRARY} DIRECTORY)
@@ -164,23 +144,19 @@ endif()
 
 find_library(BTF_LIBRARY btf
   HINTS
-    ${UMFPACK_LIB_DIR}
-    ${AMD_LIB_DIR}
-    ${COLAMD_LIB_DIR}
-    ${CHOLMOD_LIB_DIR}
-    ${SuiteSparse_DIR}/lib
-    $ENV{SuiteSparse_DIR}/lib
+  ${UMFPACK_LIB_DIR} ${AMD_LIB_DIR}
+  ${COLAMD_LIB_DIR} ${CHOLMOD_LIB_DIR}
+  ${SuiteSparse_DIR} $ENV{SuiteSparse_DIR}
+  PATH_SUFFIXES lib
+  NO_DEFAULT_PATH
   DOC "The BTF Library.")
+find_library(BTF_LIBRARY btf)
 
 if(CMAKE_VERSION VERSION_GREATER 2.8.11)
   get_filename_component(BTF_LIB_DIR ${BTF_LIBRARY} DIRECTORY)
 else()
   get_filename_component(BTF_LIB_DIR ${BTF_LIBRARY} PATH)
 endif()
-
-#message("CONFIG_INCLUDE_DIRS=${CONFIG_INCLUDE_DIRS}")
-#message("CONFIG_LIBRARY=${CONFIG_LIBRARY}")
-#message("CONFIG_LIB_DIR=${CONFIG_LIB_DIR}")
 
 set(SuiteSparse_LIBRARIES
   ${UMFPACK_LIBRARY} ${KLU_LIBRARY} ${BTF_LIBRARY} ${AMD_LIBRARY}
--- config/cmake/modules/MfemCmakeUtilities.cmake
+++ config/cmake/modules/MfemCmakeUtilities.cmake
@@ -12,23 +12,63 @@ function(add_mfem_examples EXE_SRCS)
   foreach(SRC_FILE IN LISTS ${EXE_SRCS})
     get_filename_component(SRC_FILENAME ${SRC_FILE} NAME)
     
-    string(REPLACE ".cpp" ".exe" EXE_NAME ${SRC_FILENAME})
+    string(REPLACE ".cpp" "" EXE_NAME ${SRC_FILENAME})
     add_executable(${EXE_NAME} ${SRC_FILE})
     
     target_link_libraries(${EXE_NAME} mfem)
     if (MFEM_USE_MPI)
       target_link_libraries(${EXE_NAME} ${MPI_CXX_LIBRARIES})
-    endif()
     
-    # Language-specific include directories:
-    target_include_directories(${EXE_NAME} PRIVATE ${MPI_CXX_INCLUDE_PATH})
+      # Language-specific include directories:
+      target_include_directories(${EXE_NAME} PRIVATE ${MPI_CXX_INCLUDE_PATH})
+      if (MPI_CXX_COMPILE_FLAGS)
+        target_compile_options(${EXE_NAME} PRIVATE ${MPI_CXX_COMPILE_FLAGS})
+      endif()
+      
+      if (MPI_CXX_LINK_FLAGS)
+        set_target_properties(${EXE_NAME} PROPERTIES
+          LINK_FLAGS "${MPI_CXX_LINK_FLAGS}")
+      endif()
+    endif()
+  endforeach(SRC_FILE)
+endfunction()
+
+# A slightly more versitile function for adding executables to MFEM
+function(add_mfem_executable MFEM_EXE_NAME)
+  # Parse the input arguments looking for the things we need
+  set(POSSIBLE_ARGS "MAIN" "EXTRA_SOURCES" "EXTRA_HEADERS" "EXTRA_OPTIONS" "EXTRA_DEFINES" "LIBRARIES")
+  set(CURRENT_ARG)
+  foreach(arg ${ARGN})
+    list(FIND POSSIBLE_ARGS ${arg} is_arg_name)
+    if (${is_arg_name} GREATER -1)
+      set(CURRENT_ARG ${arg})
+      set(${CURRENT_ARG}_LIST)
+    else()
+      list(APPEND ${CURRENT_ARG}_LIST ${arg})
+    endif()
+  endforeach()
+
+  # Actually add the test
+  add_executable(${MFEM_EXE_NAME} ${MAIN_LIST}
+    ${EXTRA_SOURCES_LIST} ${EXTRA_HEADERS_LIST})
+
+  # Append the additional libraries and options
+  target_link_libraries(${MFEM_EXE_NAME} PRIVATE ${LIBRARIES_LIST})
+  target_compile_options(${MFEM_EXE_NAME} PRIVATE ${EXTRA_OPTIONS_LIST})
+  target_compile_definitions(${MFEM_EXE_NAME} PRIVATE ${EXTRA_DEFINES_LIST})
+  
+  # Handle the MPI separately
+  if (MFEM_USE_MPI)
+    target_link_libraries(${MFEM_EXE_NAME} PRIVATE ${MPI_CXX_LIBRARIES})
+    
+    target_include_directories(${MFEM_EXE_NAME} PRIVATE ${MPI_CXX_INCLUDE_PATH})
     if (MPI_CXX_COMPILE_FLAGS)
-      target_compile_options(${EXE_NAME} PRIVATE ${MPI_CXX_COMPILE_FLAGS})
+      target_compile_options(${MFEM_EXE_NAME} PRIVATE ${MPI_CXX_COMPILE_FLAGS})
     endif()
     
     if (MPI_CXX_LINK_FLAGS)
-      set_target_properties(${EXE_NAME} PROPERTIES
+      set_target_properties(${MFEM_EXE_NAME} PROPERTIES
         LINK_FLAGS "${MPI_CXX_LINK_FLAGS}")
     endif()
-  endforeach(SRC_FILE)
+  endif()
 endfunction()
