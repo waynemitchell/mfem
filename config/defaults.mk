20de280d615a161cffe35d0198dd9ae5337bf3f9
Veselin Dobrev <dobrev@llnl.gov>
2016-08-23 12:47:25 -0700

    Remove '?=' assignments from config/defaults.mk and update the settings for ParMETIS needed when building with SuperLU_DIST.

--- config/defaults.mk
+++ config/defaults.mk
@@ -77,20 +77,21 @@ HYPRE_LIB = -L$(HYPRE_DIR)/lib -lHYPRE
 # METIS library configuration
 ifeq ($(MFEM_USE_SUPERLU),NO)
    ifeq ($(MFEM_USE_METIS_5),NO)
-     METIS_DIR ?= @MFEM_DIR@/../metis-4.0
-     METIS_OPT ?=
-     METIS_LIB ?= -L$(METIS_DIR) -lmetis
+     METIS_DIR = @MFEM_DIR@/../metis-4.0
+     METIS_OPT =
+     METIS_LIB = -L$(METIS_DIR) -lmetis
    else
-     METIS_DIR ?= @MFEM_DIR@/../metis-5.0
-     METIS_OPT ?= -I$(METIS_DIR)/include
-     METIS_LIB ?= -L$(METIS_DIR)/lib -lmetis
+     METIS_DIR = @MFEM_DIR@/../metis-5.0
+     METIS_OPT = -I$(METIS_DIR)/include
+     METIS_LIB = -L$(METIS_DIR)/lib -lmetis
    endif
 else
-   # ParMETIS currently needed only with SuperLU
-   METIS_DIR ?= @MFEM_DIR@/../parmetis-4.0.3
-   METIS_OPT ?=
-   METIS_LIB ?= -L$(METIS_DIR) -lparmetis -lmetis
-   MFEM_USE_METIS_5 ?= YES
+   # ParMETIS currently needed only with SuperLU. We assume that METIS 5
+   # (included with ParMETIS) is installed in the same location.
+   METIS_DIR = @MFEM_DIR@/../parmetis-4.0.3
+   METIS_OPT = -I$(METIS_DIR)/include
+   METIS_LIB = -L$(METIS_DIR)/lib -lparmetis -lmetis
+   MFEM_USE_METIS_5 = YES
 endif
 
 # LAPACK library configuration
