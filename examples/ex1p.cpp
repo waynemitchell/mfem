e778405dfb0e7e9d530ed40b99d74dc1e159a2e0
Wayne Bradford Mitchell <mitchell82@llnl.gov>
2023-03-20 12:28:26 -0700

    My changes to ex1p.cpp for experimenting with hypre soc

--- examples/ex1p.cpp
+++ examples/ex1p.cpp
@@ -65,6 +65,94 @@
 
 using namespace std;
 using namespace mfem;
+#include "HYPRE_parcsr_ls.h" 
+#include "_hypre_parcsr_ls.h" 
+
+/* *************************************** */
+/* Anisotropic diffusion coefficients */
+/* *************************************** */
+
+// Isotropic
+void alphaFunc0(const Vector &x, DenseMatrix &K)
+{
+   K.SetSize(2);
+   double theta = 0.0;
+   double eps = 1.0;
+   double c = cos(theta);
+   double s = sin(theta);
+   K(0,0) = c * c + eps * s * s;
+   K(0,1) = c * s - eps * c * s;
+   K(1,0) = c * s - eps * c * s;
+   K(1,1) = s * s + eps * c * c;
+}
+
+// Grid-aligned anisotropic
+void alphaFunc1(const Vector &x, DenseMatrix &K)
+{
+   K.SetSize(x.Size());
+   double theta = 0.0;
+   double eps = 0.0001;
+   double c = cos(theta);
+   double s = sin(theta);
+   K(0,0) = c * c + eps * s * s;
+   K(0,1) = c * s - eps * c * s;
+   K(1,0) = c * s - eps * c * s;
+   K(1,1) = s * s + eps * c * c;
+   if (K.Size() == 3)
+   {
+      K(0,2) = 0.0;
+      K(1,2) = 0.0;
+      K(2,0) = 0.0;
+      K(2,1) = 0.0;
+      K(2,2) = 1.0;
+   }
+}
+
+// Non-grid-aligned anisotropic
+void alphaFunc2(const Vector &x, DenseMatrix &K)
+{
+   K.SetSize(2);
+   double theta = 0.59; // about 3pi/16
+   double eps = 0.0001;
+   double c = cos(theta);
+   double s = sin(theta);
+   K(0,0) = c * c + eps * s * s;
+   K(0,1) = c * s - eps * c * s;
+   K(1,0) = c * s - eps * c * s;
+   K(1,1) = s * s + eps * c * c;
+}
+
+// Variable strength anisotropic
+void alphaFunc3(const Vector &x, DenseMatrix &K)
+{
+   K.SetSize(2);
+   double theta = 0.0;
+   double eps = 0.000001 + pow(x(0), 4);
+   double c = cos(theta);
+   double s = sin(theta);
+   K(0,0) = c * c + eps * s * s;
+   K(0,1) = c * s - eps * c * s;
+   K(1,0) = c * s - eps * c * s;
+   K(1,1) = s * s + eps * c * c;
+}
+
+// Variable strength and direction anisotropic
+void alphaFunc4(const Vector &x, DenseMatrix &K)
+{
+   K.SetSize(2);
+   double theta = 0.59 * pow(x(1), 4);
+   double eps = 0.000001 + pow(x(0), 4);
+   double c = cos(theta);
+   double s = sin(theta);
+   K(0,0) = c * c + eps * s * s;
+   K(0,1) = c * s - eps * c * s;
+   K(1,0) = c * s - eps * c * s;
+   K(1,1) = s * s + eps * c * c;
+}
+
+/* *************************************** */
+/* main */
+/* *************************************** */
 
 int main(int argc, char *argv[])
 {
@@ -75,14 +163,29 @@ int main(int argc, char *argv[])
    Hypre::Init();
 
    // 2. Parse command-line options.
-   const char *mesh_file = "../data/star.mesh";
+   char filename[256];
+   char mesh_path[1024] = "../data/";
+   const char *mesh_file = "star";
    int order = 1;
    bool static_cond = false;
    bool pa = false;
    bool fa = false;
    const char *device_config = "cpu";
-   bool visualization = true;
+   bool visualization = false;
+   bool plot_soln = false;
+   bool print_cf = false;
+   bool print_matrices = false;
    bool algebraic_ceed = false;
+   int cycle_type = 1;
+   int aux_S = -1;
+   int relax_type = 18;
+   int coarsen_type = 8;
+   int interp_type = 6;
+   int max_coarse_size = 6;
+   double strong_thresh = 0.25;
+   int ref_levels = 2;
+   int par_ref_levels = 0;
+   int problem = 0;
 
    OptionsParser args(argc, argv);
    args.AddOption(&mesh_file, "-m", "--mesh",
@@ -90,6 +193,24 @@ int main(int argc, char *argv[])
    args.AddOption(&order, "-o", "--order",
                   "Finite element order (polynomial degree) or -1 for"
                   " isoparametric space.");
+   args.AddOption(&cycle_type, "-c", "--cycle-type",
+                  "BoomerAMG cycle type.");
+   args.AddOption(&aux_S, "-s", "--aux-s",
+                  "BoomerAMG aux S option.");
+   args.AddOption(&relax_type, "-r", "--relax-type",
+                  "BoomerAMG relax type.");
+   args.AddOption(&coarsen_type, "-c", "--coarsen-type",
+                  "BoomerAMG coarsening type.");
+   args.AddOption(&interp_type, "-i", "--interp-type",
+                  "BoomerAMG interpolation type.");
+   args.AddOption(&max_coarse_size, "-C", "--max-coarse-size",
+                  "BoomerAMG maximum coarse grid size.");
+   args.AddOption(&strong_thresh, "-t", "--strong-thresh",
+                  "Strength of connection threshold.");
+   args.AddOption(&ref_levels, "-r", "--ref-levels",
+                  "Number of uniform mesh refinement levels on the serial mesh.");
+   args.AddOption(&par_ref_levels, "-pr", "--par-ref-levels",
+                  "Number of uniform mesh refinement levels on the parallel mesh.");
    args.AddOption(&static_cond, "-sc", "--static-condensation", "-no-sc",
                   "--no-static-condensation", "Enable static condensation.");
    args.AddOption(&pa, "-pa", "--partial-assembly", "-no-pa",
@@ -98,6 +219,8 @@ int main(int argc, char *argv[])
                   "--no-full-assembly", "Enable Full Assembly.");
    args.AddOption(&device_config, "-d", "--device",
                   "Device configuration string, see Device::Configure().");
+   args.AddOption(&problem, "-p", "--problem",
+                  "Choose the problem (changes type of anisotropy).");
 #ifdef MFEM_USE_CEED
    args.AddOption(&algebraic_ceed, "-a", "--algebraic",
                   "-no-a", "--no-algebraic",
@@ -106,6 +229,15 @@ int main(int argc, char *argv[])
    args.AddOption(&visualization, "-vis", "--visualization", "-no-vis",
                   "--no-visualization",
                   "Enable or disable GLVis visualization.");
+   args.AddOption(&plot_soln, "-ps", "--plot-soln", "-no-ps",
+                  "--no-plot-soln",
+                  "Enable or disable saving a plot of the solution to file.");
+   args.AddOption(&print_matrices, "-pm", "--print-matrices", "-no-pm",
+                  "--no-print-matrices",
+                  "Enable or disable saving of the matrices of the AMG hierarchy.");
+   args.AddOption(&print_cf, "-pcf", "--print-cf", "-no-pcf",
+                  "--no-print-cf",
+                  "Enable or disable saving of the CF splitting.");
    args.Parse();
    if (!args.Good())
    {
@@ -128,7 +260,29 @@ int main(int argc, char *argv[])
    // 4. Read the (serial) mesh from the given mesh file on all processors.  We
    //    can handle triangular, quadrilateral, tetrahedral, hexahedral, surface
    //    and volume meshes with the same code.
-   Mesh mesh(mesh_file, 1, 1);
+   Mesh mesh;
+   if ( std::strcmp(mesh_file, "square_quad") == 0 )
+   {
+      mesh = Mesh::MakeCartesian2D(16, 16, Element::QUADRILATERAL);
+   }
+   else if ( std::strcmp(mesh_file, "square_tri") == 0 )
+   {
+      mesh = Mesh::MakeCartesian2D(16, 16, Element::TRIANGLE);
+   }
+   else if ( std::strcmp(mesh_file, "cube_quad") == 0 )
+   {
+      mesh = Mesh::MakeCartesian3D(8, 8, 8, Element::QUADRILATERAL);
+   }
+   else if ( std::strcmp(mesh_file, "cube_tri") == 0 )
+   {
+      mesh = Mesh::MakeCartesian3D(8, 8, 8, Element::TRIANGLE);
+   }
+   else
+   {
+      strcat(mesh_path, mesh_file);
+      strcat(mesh_path, ".mesh");
+      mesh = Mesh(mesh_path, 1, 1);
+   }
    int dim = mesh.Dimension();
 
    // 5. Refine the serial mesh on all processors to increase the resolution. In
@@ -136,8 +290,6 @@ int main(int argc, char *argv[])
    //    'ref_levels' to be the largest number that gives a final mesh with no
    //    more than 10,000 elements.
    {
-      int ref_levels =
-         (int)floor(log(10000./mesh.GetNE())/log(2.)/dim);
       for (int l = 0; l < ref_levels; l++)
       {
          mesh.UniformRefinement();
@@ -150,7 +302,6 @@ int main(int argc, char *argv[])
    ParMesh pmesh(MPI_COMM_WORLD, mesh);
    mesh.Clear();
    {
-      int par_ref_levels = 2;
       for (int l = 0; l < par_ref_levels; l++)
       {
          pmesh.UniformRefinement();
@@ -200,6 +351,27 @@ int main(int argc, char *argv[])
       fespace.GetEssentialTrueDofs(ess_bdr, ess_tdof_list);
    }
 
+   /* WM: print out the coordinates for the dofs */
+   ofstream outfile;
+   if (print_cf)
+   {
+      sprintf(filename, "outputs/coords_%s_ref%d_prob%d", mesh_file, ref_levels, problem);
+      outfile.open(filename);
+      ParFiniteElementSpace coords_fespaces(&pmesh, fec, dim);
+      ParGridFunction nodes(&coords_fespaces);
+      pmesh.GetNodes(nodes);
+      const int nNodes = nodes.Size() / dim;
+      for (int i = 0; i < nNodes; ++i)
+      {
+         for (int j = 0; j < dim; ++j)
+         {
+            outfile << nodes(j * nNodes + i) << " "; 
+         }   
+         outfile << endl;
+      }
+   }
+   outfile.close();
+
    // 9. Set up the parallel linear form b(.) which corresponds to the
    //    right-hand side of the FEM linear system, which in this case is
    //    (1,phi_i) where phi_i are the basis functions in fespace.
@@ -227,7 +399,28 @@ int main(int argc, char *argv[])
       // bit-for-bit deterministic at the cost of somewhat longer run time.
       a.EnableSparseMatrixSorting(Device::IsEnabled());
    }
-   a.AddDomainIntegrator(new DiffusionIntegrator(one));
+   MatrixFunctionCoefficient *alpha;
+   if (problem == 1)
+   {
+      alpha = new MatrixFunctionCoefficient(dim, alphaFunc1);
+   }
+   else if (problem == 2)
+   {
+      alpha = new MatrixFunctionCoefficient(dim, alphaFunc2);
+   }
+   else if (problem == 3)
+   {
+      alpha = new MatrixFunctionCoefficient(dim, alphaFunc3);
+   }
+   else if (problem == 4)
+   {
+      alpha = new MatrixFunctionCoefficient(dim, alphaFunc4);
+   }
+   else
+   {
+      alpha = new MatrixFunctionCoefficient(dim, alphaFunc0);
+   }
+   a.AddDomainIntegrator(new DiffusionIntegrator(*alpha));
 
    // 12. Assemble the parallel bilinear form and the corresponding linear
    //     system, applying any necessary transformations such as: parallel
@@ -240,35 +433,54 @@ int main(int argc, char *argv[])
    Vector B, X;
    a.FormLinearSystem(ess_tdof_list, x, b, A, X, B);
 
+   /* WM: randomize RHS */
+   B.Randomize();
+
    // 13. Solve the linear system A X = B.
    //     * With full assembly, use the BoomerAMG preconditioner from hypre.
    //     * With partial assembly, use Jacobi smoothing, for now.
-   Solver *prec = NULL;
-   if (pa)
-   {
-      if (UsesTensorBasis(fespace))
-      {
-         if (algebraic_ceed)
-         {
-            prec = new ceed::AlgebraicSolver(a, ess_tdof_list);
-         }
-         else
-         {
-            prec = new OperatorJacobiSmoother(a, ess_tdof_list);
-         }
-      }
-   }
-   else
-   {
-      prec = new HypreBoomerAMG;
-   }
+   // WM: only using the BoomerAMG preconditioner
+   HypreBoomerAMG *prec = NULL;
+   prec = new HypreBoomerAMG;
+   prec->SetStrengthThresh(strong_thresh);
+   prec->SetAggressiveCoarsening(0);
+
+   HYPRE_Solver hypre_solver = *prec;
+   HYPRE_BoomerAMGSetCycleType(hypre_solver, cycle_type);
+   HYPRE_BoomerAMGSetUseAuxStrengthMatrix(hypre_solver, aux_S);
+   if (relax_type >= 0) HYPRE_BoomerAMGSetRelaxType(hypre_solver, relax_type);
+   if (print_matrices) HYPRE_BoomerAMGSetPrintLevel(hypre_solver, 3);
+   HYPRE_BoomerAMGSetMaxRowSum(hypre_solver, 1.0);
+   HYPRE_BoomerAMGSetCoarsenType(hypre_solver, coarsen_type);
+   HYPRE_BoomerAMGSetInterpType(hypre_solver, interp_type);
+   HYPRE_BoomerAMGSetTruncFactor(hypre_solver, 0.0);
+   HYPRE_BoomerAMGSetMaxCoarseSize(hypre_solver, max_coarse_size);
+
    CGSolver cg(MPI_COMM_WORLD);
-   cg.SetRelTol(1e-12);
+   cg.SetRelTol(1e-8);
    cg.SetMaxIter(2000);
    cg.SetPrintLevel(1);
    if (prec) { cg.SetPreconditioner(*prec); }
    cg.SetOperator(*A);
    cg.Mult(B, X);
+
+   /* WM: print out CF splitting */
+   if (print_cf)
+   {
+      hypre_ParAMGData *amg_solver = (hypre_ParAMGData*) hypre_solver;
+      for (int level = 0; level < hypre_ParAMGDataNumLevels(amg_solver) - 1; level++)
+      {
+         sprintf(filename, "outputs/cf_marker_%s_ref%d_prob%d_str%f_auxs%d_lvl%d", mesh_file, ref_levels, problem, strong_thresh, aux_S, level);
+         outfile.open(filename);
+         hypre_IntArray *CF_marker = hypre_ParAMGDataCFMarkerArray(amg_solver)[level];
+         for (int i = 0; i < hypre_IntArraySize(CF_marker); i++)
+         {
+            outfile << hypre_IntArrayData(CF_marker)[i] << "\n";
+         }
+         outfile.close();
+      }
+   }
+
    delete prec;
 
    // 14. Recover the parallel grid function corresponding to X. This is the
@@ -277,10 +489,11 @@ int main(int argc, char *argv[])
 
    // 15. Save the refined mesh and the solution in parallel. This output can
    //     be viewed later using GLVis: "glvis -np <np> -m mesh -g sol".
+   if (plot_soln)
    {
       ostringstream mesh_name, sol_name;
-      mesh_name << "mesh." << setfill('0') << setw(6) << myid;
-      sol_name << "sol." << setfill('0') << setw(6) << myid;
+      mesh_name << "mesh" << problem << "." << setfill('0') << setw(6) << myid;
+      sol_name << "sol" << problem << "." << setfill('0') << setw(6) << myid;
 
       ofstream mesh_ofs(mesh_name.str().c_str());
       mesh_ofs.precision(8);
@@ -301,6 +514,8 @@ int main(int argc, char *argv[])
       sol_sock.precision(8);
       sol_sock << "solution\n" << pmesh << x << flush;
    }
+
+   cout << "Number of finite element unknowns: " << size << endl;
 
    // 17. Free the used memory.
    if (delete_fec)
